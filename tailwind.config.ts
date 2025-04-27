7d9da040fca0f8d872230dd2148f71abe0bb20b1
Shinnφ <166776356+Shinniam@users.noreply.github.com>
2025-04-28 07:46:48 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,21 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  content: ['./app/**/*.{ts,tsx}'],
-  darkMode: 'media',
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shimmer: {
+          '0%': { backgroundPosition: '-1000px 0' },
+          '100%': { backgroundPosition: '1000px 0' },
+        },
+      },
+      animation: {
+        shimmer: 'shimmer 2s infinite linear',
+      },
+    },
   },
   plugins: [],
 };
-
-export default config;
